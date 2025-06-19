bb0d5e2aeec036a0d32005744eda23d8a99f45a9
PAWU11 <ptik@outlook.de>
2025-06-19 15:54:52 +0200

    Update Linux_Debian_SecurityUpdate.sh

--- Linux_Debian_SecurityUpdate.sh
+++ Linux_Debian_SecurityUpdate.sh
@@ -1,1 +1,15 @@
 
+#!/bin/bash
+
+echo "Aktualisiere Paketlisten (inkl. Sicherheitsupdates)..."
+apt update -o Dir::Etc::sourcelist="sources.list.d/debian-security.list" \
+           -o Dir::Etc::sourceparts="-" \
+           -o APT::Get::List-Cleanup="0"
+
+echo "Installiere nur Sicherheitsupdates..."
+apt list --upgradable 2>/dev/null | grep -i security
+
+apt upgrade --with-new-pkgs --only-upgrade \
+     -o Dir::Etc::sourcelist="sources.list.d/debian-security.list" \
+     -o Dir::Etc::sourceparts="-" \
+     -o APT::Get::List-Cleanup="0" -y
